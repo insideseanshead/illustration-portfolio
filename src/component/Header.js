abf6830f540f35ca4c4c98f220c51f1746f3ec5b
Sean Morgan <insideseanshead@gmail.com>
2021-01-18 20:20:42 -0800

    added nav

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,20 +1,47 @@
-import 'react-bulma-components/dist/react-bulma-components.min.css';
-import { Hero } from 'react-bulma-components';
+import "react-bulma-components/dist/react-bulma-components.min.css";
+import { Hero, Navbar } from "react-bulma-components";
 
 const Header = () => {
   return (
-    <section className="hero is-success is-fullheight">
-  <div className="hero-body">
-    <div className="container">
-      <h1 className="title">
-        Fullheight title
-      </h1>
-      <h2 className="subtitle">
-        Fullheight subtitle
-      </h2>
-    </div>
+    <>
+      <section className="hero is-success is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Sean Morgan</h1>
+            <h2 className="subtitle">Illustration</h2>
+          </div>
+        </div>
+      </section>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
   </div>
-</section>
+
+  <div id="navbarBasicExample" className="navbar-menu">
+    <div className="navbar-start">
+      <a className="navbar-item">
+        Home
+      </a>
+
+      <a className="navbar-item">
+        Documentation
+      </a>
+
+      
+    </div>
+
+    
+  </div>
+</nav>
+    </>
   );
 };
 
