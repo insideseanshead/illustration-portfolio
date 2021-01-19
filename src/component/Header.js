7b5cafa2a7ef7dc0ba192bf39fecb09ffcf7acd6
Sean Morgan <insideseanshead@gmail.com>
2021-01-19 11:55:58 -0800

    created custom navbar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,6 @@
 import "react-bulma-components/dist/react-bulma-components.min.css";
 import { Hero, Navbar } from "react-bulma-components";
+import Nav from "./Nav"
 
 const Header = () => {
   return (
@@ -12,41 +13,7 @@ const Header = () => {
           </div>
         </div>
       </section>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
-    </a>
-
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" className="navbar-menu">
-    <div className="navbar-start">
-      <a className="navbar-item">
-        Illustrations
-      </a>
-
-      <a className="navbar-item">
-        Contact
-      </a>
-
-      <a className="navbar-item">
-        Twitter
-      </a>
-
-      <a className="navbar-item">
-        Instagram
-      </a>
-    </div>
-
-    
-  </div>
-</nav>
+      <Nav />
     </>
   );
 };
