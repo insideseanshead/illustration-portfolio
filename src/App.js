1e978b5d1202cb4c1bcd052fb79e323b9bf6e608
Sean Morgan <insideseanshead@gmail.com>
2021-01-19 13:40:56 -0800

    Created contact component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './component/Header'
 import Gallery from './component/Gallery'
+import Contact from './component/Contact'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Gallery />
+      <Contact />
     </div>
   );
 }
