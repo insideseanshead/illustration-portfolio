36a17cba099a758a44c244059a41cbb575145e1d
Sean Morgan <insideseanshead@gmail.com>
2021-01-19 13:28:51 -0800

    got sticky nav working.

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,53 +1,67 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 
 const Nav = () => {
+
+// sticky navbar
+  const [sticky, setSticky] = React.useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 900) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  let navbarClasses = ["nav"];
+
+  if (sticky) {
+    navbarClasses.push("sticky");
+  }
+
   return (
-      <nav>
-          <ul>
-              <li><a href="#">Illustration</a></li>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">Twitter</a></li>
-              <li><a href="#">Instagram</a></li>
-          </ul>
+    <div className={navbarClasses.join(" ")}>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              width="112"
+              height="28"
+            />
+          </a>
+
+          <a
+            role="button"
+            className="navbar-burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu is-fixed-top">
+          <div className="navbar-start">
+            <a className="navbar-item">Illustrations</a>
+
+            <a className="navbar-item">Contact</a>
+
+            <a className="navbar-item">Twitter</a>
+
+            <a className="navbar-item">Instagram</a>
+          </div>
+        </div>
       </nav>
-    // <div>
-    //   <nav className="navbar" role="navigation" aria-label="main navigation">
-    //     <div className="navbar-brand">
-    //       <a className="navbar-item" href="https://bulma.io">
-    //         <img
-    //           src="https://bulma.io/images/bulma-logo.png"
-    //           width="112"
-    //           height="28"
-    //         />
-    //       </a>
-
-    //       <a
-    //         role="button"
-    //         className="navbar-burger"
-    //         aria-label="menu"
-    //         aria-expanded="false"
-    //         data-target="navbarBasicExample"
-    //       >
-    //         <span aria-hidden="true"></span>
-    //         <span aria-hidden="true"></span>
-    //         <span aria-hidden="true"></span>
-    //       </a>
-    //     </div>
-
-    //     <div id="navbarBasicExample" className="navbar-menu is-fixed-top">
-    //       <div className="navbar-start">
-    //         <a className="navbar-item">Illustrations</a>
-
-    //         <a className="navbar-item">Contact</a>
-
-    //         <a className="navbar-item">Twitter</a>
-
-    //         <a className="navbar-item">Instagram</a>
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </div>
+    </div>
   );
 };
 
