11a83efd205614a1c187cf36cde494d6b83d5d7b
Sean Morgan <insideseanshead@gmail.com>
2021-01-22 14:22:45 -0800

    updated images in app.js state

--- src/App.js
+++ src/App.js
@@ -8,77 +8,77 @@ function App() {
   const [images, setImages] = useState([
     {
         id: 1,
-        image:'https://seanmorganillustration.com/images/main/m_hellblazer.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_hellblazer.jpg',
         alt:'Illustration Based on Gaslands Game.'
     },
     {
         id: 2,
-        image:'https://seanmorganillustration.com/images/main/m_fourarm.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_fourarm.jpg',
         alt:'Illustration for a draw in your style competition.'
     },
     {
         id: 3,
-        image:'https://seanmorganillustration.com/images/main/m_sam_simian.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_sam_simian.jpg',
         alt:'Character Design project for TTRPG.'
     },
     {
         id: 4,
-        image:'https://seanmorganillustration.com/images/main/m_tabby.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_tabby.jpg',
         alt:'Gold Dragonborn Barbarian Character Commission.'
     },
     {
         id: 5,
-        image:'https://seanmorganillustration.com/images/main/m_reilly.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_reilly.jpg',
         alt:'Dungeons and Dragons character design commission.'
     },
     {
         id: 6,
-        image:'https://seanmorganillustration.com/images/main/m_gaslands.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_gaslands.jpg',
         alt:'Illustration for Gaslands Game.'
     },
     {
         id: 7,
-        image:'https://seanmorganillustration.com/images/main/m_home.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_home.jpg',
         alt:'Self Portrait.'
     },
     {
         id: 8,
-        image:'https://seanmorganillustration.com/images/main/m_medusa.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_medusa.jpg',
         alt:'Medusa Character Design.'
     },
     {
         id: 9,
-        image:'https://seanmorganillustration.com/images/main/m_kirk.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_kirk.jpg',
         alt:'Captain Kirk Portrait.'
     },
     {
         id: 10,
-        image:'https://seanmorganillustration.com/images/main/m_missing_cleavland.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_missing_cleavland.jpg',
         alt:'Missing Cleavland.'
     },
     {
         id: 11,
-        image:'https://seanmorganillustration.com/images/main/m_lindariel.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_lindariel.jpg',
         alt:'Elf Ranger Linariel Character Design.'
     },
     {
         id: 12,
-        image:'https://seanmorganillustration.com/images/main/m_strangeways.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_strangeways.jpg',
         alt:'Strange Ways.'
     },
     {
         id: 13,
-        image:'https://seanmorganillustration.com/images/main/m_hellboy.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_hellboy.jpg',
         alt:'Hellboy Illustration.'
     },
     {
         id: 14,
-        image:'https://seanmorganillustration.com/images/main/m_skull_and_fez_new.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_skull_and_fez_new.jpg',
         alt:'Tentiskull design.'
     },
     {
         id: 15,
-        image:'https://seanmorganillustration.com/images/main/m_studyingkills.jpg',
+        image:'http://seanmorganillustration.com/images/main/m_studyingkills.jpg',
         alt:'Studying Kills book cover illustration.'
     }
 ])
