c09fe9405b8d72df04c47ab98525cddb4a3ac7e8
Sean Morgan <insideseanshead@gmail.com>
2021-02-18 20:19:12 -0800

    edited img call from cloudinary to optimize image size

--- src/component/Image.js
+++ src/component/Image.js
@@ -4,7 +4,7 @@ import React from "react";
 const Image = ({ image }) => {
   return (
     <div className="thumbnail">
-      <img className="content" src={`https://res.cloudinary.com/seanmorgandesigns/image/upload/${image.public_id}`} alt={image.context.custom.alt} />
+      <img className="content" src={`https://res.cloudinary.com/seanmorgandesigns/image/upload/f_auto,q_auto/${image.public_id}`} alt={image.context.custom.alt} />
     </div>
   );
 };
